8286d091e54943b684fb11428f852a26cc4ddd27
HunterBMT <hunterbmt@gmail.com>
2013-03-09 18:32:07 +0700

    edit and recheck sql script
    Sửa lỗi sql script + gen diagram

--- src/main/resources/db/SQLScript.sql
+++ src/main/resources/db/SQLScript.sql
@@ -1,11 +1,43 @@
+CREATE TABLE `tbl_users`(
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+)  ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+CREATE TABLE `tbl_user_info`(
+   `user_id` int(10) NOT NULL,
+   `fullname`  varchar(255) NOT NULL,
+   `birthday` DATETIME NOT NULL,
+   `address` varchar(255) NOT NULL,
+   `phone` varchar(255) NOT NULL,
+  PRIMARY KEY (`user_id`),
+  CONSTRAINT `info_user_fkey` FOREIGN KEY (`user_id`) REFERENCES `tbl_users`(`id`) ON DELETE CASCADE ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=UTF8;
+
+
+CREATE TABLE `tbl_user_payment`(
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `user_id` int(10) NOT NULL,
+  `payment_day` DATETIME NOT NULL,
+  `card_code` VARCHAR(20) NOT NULL,
+  `amount` int(10) NOT NULL,
+  PRIMARY KEY (`id`),
+  CONSTRAINT `payment_fkey` FOREIGN KEY (`user_id`) REFERENCES `tbl_users`(`id`) ON DELETE CASCADE ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+
 CREATE TABLE `tbl_category` (
     `id` int(10) NOT NULL AUTO_INCREMENT,
     `category_name` varchar(50) NOT NULL,
     `description` varchar(100) NOT NULL,
     PRIMARY KEY (`id`)
-);	
-CREATE TABLE `tbl_Product` (
-    `product_id` int(10) NOT NULL AUTO_INCREMENT,
+);
+	
+CREATE TABLE `tbl_product` (
+    `id` int(10) NOT NULL AUTO_INCREMENT,
     `product_name` varchar(100) NOT NULL,
     `category_id` int(10) NOT NULL,
     `description` varchar(400) NOT NULL,
@@ -15,11 +47,23 @@ CREATE TABLE `tbl_Product` (
     `max_price` DOUBLE NOT NULL,
     `last_update` DATETIME DEFAULT NULL,
     `is_active` TINYINT(1) NOT NULL,
-    PRIMARY KEY (`product_id`),
+    PRIMARY KEY (`id`),
     CONSTRAINT `product_fkey` FOREIGN KEY (`category_id`)
         REFERENCES `tbl_category` (`id`)
         ON DELETE CASCADE ON UPDATE NO ACTION
-)  ENGINE=InnoDB DEFAULT CHARSET=latin1;
+)  ENGINE=InnoDB DEFAULT CHARSET=UTF8;
+
+CREATE TABLE `tbl_order_history`(
+   `id` int(10) NOT NULL AUTO_INCREMENT,
+   `user_id` int(10) NOT NULL,
+   `product_id` int(10) NOT NULL,
+   `order_day` DATETIME NOT NULL,
+   `address` varchar(255) NOT NULL,
+   `amount` int(10) NOT NULL,
+  PRIMARY KEY (`id`),
+  CONSTRAINT `order_user_fkey` FOREIGN KEY (`user_id`) REFERENCES `tbl_users`(`id`) ON DELETE CASCADE ON UPDATE NO ACTION,
+  CONSTRAINT `order_product_fkey` FOREIGN KEY (`product_id`) REFERENCES  `tbl_product` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 CREATE TABLE `tbl_tags` (
     `id` int(10) NOT NULL AUTO_INCREMENT,
@@ -37,7 +81,7 @@ CREATE TABLE `tbl_tags_product` (
         REFERENCES `tbl_tags` (`id`)
         ON DELETE CASCADE ON UPDATE NO ACTION,
     CONSTRAINT `product_tags_fkey` FOREIGN KEY (`product_id`)
-        REFERENCES `tbl_product` (`product_id`)
+        REFERENCES `tbl_product` (`id`)
         ON DELETE CASCADE ON UPDATE NO ACTION
 )  ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -50,8 +94,8 @@ CREATE TABLE `tbl_bids` (
     `end_date` DATETIME DEFAULT NULL,
     `last_edit` DATETIME DEFAULT NULL,
     PRIMARY KEY (`id`),
-    CONSTRAINT `product_fkey` FOREIGN KEY (`product_id`)
-        REFERENCES `tbl_products` (`id`)
+    CONSTRAINT `bids_product_fkey` FOREIGN KEY (`product_id`)
+        REFERENCES `tbl_product` (`id`)
         ON DELETE CASCADE ON UPDATE NO ACTION
 )  ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -62,51 +106,20 @@ CREATE TABLE `tbl_bid_history` (
     `price` double NOT NULL,
     bid_time DATETIME DEFAULT NULL,
     PRIMARY KEY (`id`),
-    CONSTRAINT `user_fkey` FOREIGN KEY (`user_id`)
+    CONSTRAINT `bid_history_user_fkey` FOREIGN KEY (`user_id`)
         REFERENCES `tbl_users` (`id`)
         ON DELETE CASCADE ON UPDATE NO ACTION,
-    CONSTRAINT `bid_fkey` FOREIGN KEY (`bid_id`)
+    CONSTRAINT `bid_history_bid_fkey` FOREIGN KEY (`bid_id`)
         REFERENCES `tbl_bids` (`id`)
         ON DELETE CASCADE ON UPDATE NO ACTION
 )  ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-CREATE TABLE tbl_users(
-  user_id int(10) NOT NULL AUTO_INCREMENT,
-  email varchar(255) NOT NULL,
-  password varchar(255) NOT NULL,
-  PRIMARY KEY (uuid)
-)  ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-CREATE TABLE tbl_user_payments(
-   payment_id int(10) NOT NULL AUTO_INCREMENT,
-   fullname  varchar(255) NOT NULL,
-    birthday DATETIME NOT NULL,
-    address varchar(255) NOT NULL,
-   phone varchar(255) NOT NULL,
-   user_id int(10) NOT NULL,
-  PRIMARY KEY (_payment_id),
-  CONSTRAINT info_fkey FOREIGN KEY (user_id) REFERENCES tbl_users(user_id) ON DELETE CASCADE ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-
-CREATE TABLE tbl_user_payment(
-   user_payment_id int(10) NOT NULL AUTO_INCREMENT,
-   user_id int(10) NOT NULL,
-  payment_day DATETIME NOT NULL,
-   card_code text NOT NULL,
-   amount int(10) NOT NULL,
-  PRIMARY KEY (user_payment_id),
-  CONSTRAINT payment_fkey FOREIGN KEY (user_id) REFERENCES tbl_users(user_id) ON DELETE CASCADE ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-CREATE TABLE tbl_order_history(
-   order_history_id int(10) NOT NULL AUTO_INCREMENT,
-   user_id int(10) NOT NULL,
-   product_id int(10) NOT NULL,
-   order_day DATETIME NOT NULL,
-   address varchar(255) NOT NULL,
-   amount int(10) NOT NULL,
-  PRIMARY KEY (order_history_id),
-  CONSTRAINT order_user_fkey FOREIGN KEY (user_id) REFERENCES tbl_users(user_id) ON DELETE CASCADE ON UPDATE NO ACTION,
-  CONSTRAINT order_product_fkey FOREIGN KEY (product_id) REFERENCES  tbl_Product (product_id) ON DELETE CASCADE ON UPDATE NO ACTION
+CREATE TABLE `tbl_card_code`(
+   `id` int(10) NOT NULL AUTO_INCREMENT,
+   `code` varchar(20) NOT NULL,
+   `used_by` int(10) NULL,
+   `used_day` DATETIME NULL,
+   `amount` int(10) NOT NULL,
+  PRIMARY KEY (`id`),
+  CONSTRAINT `code_user_fkey` FOREIGN KEY (`used_by`) REFERENCES `tbl_users`(`id`) ON DELETE CASCADE ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
