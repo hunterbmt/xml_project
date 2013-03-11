a5c129bdc766ea594eb0133a0e3d0bfec03bd59a
HunterBMT <hunterbmt@gmail.com>
2013-03-11 14:44:35 +0700

    new file:   src/main/java/com/vteam/xml_project/controller/AdminController.java 	deleted:    src/main/java/com/vteam/xml_project/hibernate/orm/User.java 	new file:   src/main/webapp/WEB-INF/views/admin_product.jsp 	modified:   src/main/java/com/vteam/xml_project/api/UserAPI.java 	modified:   src/main/java/com/vteam/xml_project/controller/AdminController.java 	modified:   src/main/java/com/vteam/xml_project/hibernate/dao/UserDAO.java 	modified:   src/main/webapp/WEB-INF/views/admin.jsp 	modified:   src/main/webapp/WEB-INF/views/admin_product.jsp 	modified:   src/main/webapp/WEB-INF/views/newhtml.html 	modified:   src/main/webapp/WEB-INF/views/newjsp.jsp 	src/main/java/com/vteam/xml_project/hibernate/orm/ 	src/main/resources/hibernate.cfg.xml 	src/main/resources/hibernate.reveng.xml

---
+++ src/main/java/com/vteam/xml_project/controller/AdminController.java
@@ -0,0 +1,59 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.vteam.xml_project.controller;
+
+import com.vteam.xml_project.service.UserService;
+import javax.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ *
+ * @author Administrator
+ */
+@Controller
+public class MainController {
+
+    @Autowired
+    private UserService userService;
+    @Autowired
+    private UserSession session;
+
+    @RequestMapping(value = "/")
+    @Transactional
+    public ModelAndView main(HttpServletRequest request) {
+        ModelAndView mav = new ModelAndView("main");
+        return mav;
+    }
+
+    @RequestMapping(value = "/admin")
+    @Transactional
+    public ModelAndView admin(HttpServletRequest request) {
+        ModelAndView mav = new ModelAndView("admin");
+        return mav;
+    }
+    @RequestMapping(value = "/routes")
+    @Transactional
+    public ModelAndView routes(HttpServletRequest request) {
+        ModelAndView mav = new ModelAndView("routes");
+        return mav;
+    }
+    @RequestMapping(value = "/catalogs")
+    @Transactional
+    public ModelAndView catalogs(HttpServletRequest request) {
+        ModelAndView mav = new ModelAndView("catalogs");
+        return mav;
+    }
+    @RequestMapping(value = "/keywords")
+    @Transactional
+    public ModelAndView keywords(HttpServletRequest request) {
+        ModelAndView mav = new ModelAndView("keywords");
+        return mav;
+    }
+    
+}
