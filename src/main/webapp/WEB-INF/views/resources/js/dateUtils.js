940816de9645a0424e464e01bf9245eb22927203
crick <evrthg2you@gmail.com>
2013-03-18 23:28:32 +0700

    1
    1

--- src/main/webapp/WEB-INF/views/resources/js/dateUtils.js
+++ src/main/webapp/WEB-INF/views/resources/js/dateUtils.js
@@ -23,30 +23,37 @@ function formatDate(pubDate) {
     var monthname = new Array("Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec");
     var formattedDate = weekday[pubDate.getDay()] + ' '
             + monthname[pubDate.getMonth()] + ' '
-            + pubDate.getDate() + ', ' + pubDate.getFullYear() ;
+            + pubDate.getDate() + ', ' + pubDate.getFullYear();
     return formattedDate;
 }
 
 function formatDate1(ts) {
     var pubDate = new Date(ts);
-    var formattedDate = 
-            (pubDate.getMonth()+1) + '/' + (pubDate.getDate()) + '/'
-            +  pubDate.getFullYear() + ' ' + getTime(ts);
+    var formattedDate =
+            (pubDate.getMonth() + 1) + '/' + (pubDate.getDate()) + '/'
+            + pubDate.getFullYear() + ' ' + getTime(ts);
     return formattedDate;
 }
 
 
 function getTime(ts) {
     var date = new Date(ts);
-// hours part from the timestamp
-var hours = date.getHours();
-// minutes part from the timestamp
-var minutes = date.getMinutes();
-// seconds part from the timestamp
-var seconds = date.getSeconds();
 
-// will display time in 10:30:23 format
-var formattedTime = hours + ':' + minutes + ':' + seconds;
+    var hours = date.getHours();
 
-return formattedTime;
+    var minutes = date.getMinutes();
+
+    var seconds = date.getSeconds();
+    if (hours < 10)
+        hours = '0' + hours;
+
+    if (minutes < 10)
+        minutes = '0' + minutes;
+
+    if (seconds < 10)
+        seconds = '0' + seconds;
+
+    var formattedTime = hours + ':' + minutes + ':' + seconds;
+
+    return formattedTime;
 }
