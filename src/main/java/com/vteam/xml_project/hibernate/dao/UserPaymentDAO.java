fd4c95a26636bd79ca4455fda2a22d9511e82bdc
HunterBMT <hunterbmt@gmail.com>
2013-03-28 13:23:16 +0700

    change jquery to pure js

---
+++ src/main/java/com/vteam/xml_project/hibernate/dao/UserPaymentDAO.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.vteam.xml_project.hibernate.dao;
+
+import org.springframework.stereotype.Repository;
+
+/**
+ *
+ * @author TH11032013
+ */
+@Repository
+public class UserPaymentDAO extends BaseDAO{
+}
--- src/main/webapp/WEB-INF/views/resources/js/vteam.js
+++ src/main/webapp/WEB-INF/views/resources/js/vteam.js
@@ -1,15 +1,15 @@
 var vteam_http = {
-    init: function () {
+    init: function() {
         vteam_http.req = new XMLHttpRequest();
     },
-    makeHttpRequest: function (url, params, method, callbackFunction) {
+    makeHttpRequest: function(url, params, method, callbackFunction) {
         vteam_http.req = new XMLHttpRequest();
         var http = vteam_http.req;
         var queryString = converJSonToQueryString(params);
         if (method.toUpperCase() == 'GET') {
             http.open(
-                "GET", url + "?" + queryString, true);
-            http.onreadystatechange = function () {
+                    "GET", url + "?" + queryString, true);
+            http.onreadystatechange = function() {
                 if (http.readyState == 4 && http.status == 200) {
                     callbackFunction(eval("(" + http.response + ")"));
                 }
@@ -18,7 +18,7 @@ var vteam_http = {
         } else if (method.toUpperCase() == 'POST') {
             http.open("POST", url, true);
             http.setRequestHeader("Content-type", "application/x-www-form-urlencoded; charset=UTF-8");
-            http.onreadystatechange = function () {
+            http.onreadystatechange = function() {
                 if (http.readyState == 4 && http.status == 200) {
                     callbackFunction(eval("(" + http.response + ")"));
                 }
@@ -27,14 +27,63 @@ var vteam_http = {
         } else {
             throw new Error("Unsupport this method");
         }
+    },
+    getByID: function(id) {
+        return document.getElementById(id);
+    },
+    hasClass: function(el, cl) {
+        var regex = new RegExp('(?:\\s|^)' + cl + '(?:\\s|$)');
+        return !!el.className.match(regex);
+    },
+    addClass: function(id, cl) {
+
+        el = this.getByID(id);
+        if (!this.hasClass(el, cl)) {
+            el.className += ' ' + cl;
+        }
+    },
+    removeClass: function(id, cl) {
+        el = this.getByID(id);
+        var regex = new RegExp('(?:\\s|^)' + cl + '(?:\\s|$)', "gi");
+        el.className = el.className.replace(regex, ' ');
+    },
+    toggleClass: function(el, cl) {
+        hasClass(el, cl) ? removeClass(el, cl) : addClass(el, cl);
+
+    },
+    show: function(id) {
+        this.removeClass(id, "hide");
+    },
+    hide: function(id) {
+        this.addClass(id, "hide");
+    },
+    setHTML: function(id, html) {
+        el = this.getByID(id);
+        el.innerHTML = html;
+    },
+    getHTML: function(id) {
+        el = this.getByID(id);
+        return el.innerHTML;
+    },
+    appendTo: function(id, html) {
+        el = this.getByID(id);
+        el.innerHTML = el.innerHTML + html;
+    },
+    getValue: function(id) {
+        el = this.getByID(id);
+        return el.value;
+    },
+    setValue: function(id, value) {
+        el = this.getByID(id);
+        el.value = value;
     }
 }
 
-    function converJSonToQueryString(json) {
-        str = '';
-        for (key in json) {
-            str += key + '=' + json[key] + '&';
-        }
-        str = str.slice(0, str.length - 1);
-        return str;
+function converJSonToQueryString(json) {
+    str = '';
+    for (key in json) {
+        str += key + '=' + json[key] + '&';
     }
+    str = str.slice(0, str.length - 1);
+    return str;
+}
